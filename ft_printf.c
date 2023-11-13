5d3f71df6a8fb252097a171e44b52c6df5af718a
Ryan Boudwin <rboudwin@student.hive.fi>
2023-11-13 15:36:30 +0200

    rewrote printf to print as we go instead of trying to assemble one long string to print all at once

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: rboudwin <rboudwin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/09 15:32:55 by rboudwin          #+#    #+#             */
-/*   Updated: 2023/11/13 14:06:24 by rboudwin         ###   ########.fr       */
+/*   Updated: 2023/11/13 15:35:32 by rboudwin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,89 +29,43 @@
  *
  * We do not have to implement buffer management
  * */
-#include "libft_ftprintf.h"
+#include "ft_printf.h"
 #include <stdio.h>
 
-static char	*ft_identify_data_type(const char *c, char *str, int *i, va_list args)
+int	ft_identify_data_type(char const *c, int i, va_list args)
 {
-	int		n;
-	char	*tmpstr;
-	char	*itoa_result;
+	int n;
 
-	printf("We are trying to id data type, i is currently %d and c[i] is '%c'", *i, c[*i]);
-	(*i)++;
-
-	if (c[*i] == 'd')
+	i++;
+	if (c[i] == 'd')
 	{
 		n = va_arg(args, int);
-		printf("n is %d \n", n);
-		itoa_result = ft_itoa(n);
-		tmpstr = ft_strjoin(str, itoa_result);
-		free(itoa_result);
-		free(str);
-		str = tmpstr;
-		(*i)++;
-		return (str);
+		ft_putnbr_fd(n, 1);
+		return(i);
 	}
-
-	return (str);
+	return (i);
 }
-int ft_printf(const char *c, ...)
+
+int	ft_printf(const char *c, ...)
 {
 	int		i;
-	int		start;
-	int		len;
-	char	*str;	
-	char	*sub_str;
 	va_list	args;
-	char	*tmpstr;
+	int len;
 
 	i = 0;
-	start = 0;
 	len = 0;
-	str = malloc(1);
-	str[0] = '\0';
-	printf("%s \n", str);
 	va_start(args, c);
-
-printf("ft_printf launched, str is now '%s' \n", str);
 	while (c[i] != '\0')
 	{
 		if (c[i] == '%')
-		{
-			printf("we entered the percent loop \n");
-			sub_str = ft_substr(c, start, len);
-			tmpstr = ft_strjoin(str, sub_str);
-			free(sub_str); 
-			free(str);
-			if (!tmpstr)
-				return (-1);
-			str = tmpstr;
-			free(tmpstr);
-			// If we decide to write logic to handle flags, we will need to wrap the next line with a
-			// flag handler
-			//va_arg(args, char *);
-			tmpstr = ft_identify_data_type(c, str, &i, args);
-			if (!tmpstr)
-				return (-1);
-			str = tmpstr;
-			c = &c[i];
-		}
+			i = ft_identify_data_type(c, i, args);
 		else
+		{
+			(ft_putchar_fd(c[i], 1));
 			len++;
+		}
 		i++;
 	}
-	printf("We exited format string loop, i is '%d' and str is '%s' \n", i, str);
-	tmpstr = ft_strjoin(str, (char *)c);
-	if (str[0] == '\0')
-		free(str);
-	str = tmpstr;
-	printf("We are done checking to see if str still has a null terminator \n");
-	ft_putstr_fd(str, 1);
-	// printf("\n It works %s\n", str);
-	// ft_putstr_fd(str, 1);
 	va_end(args);
-	len = ft_strlen(str);
-	free(str);
 	return (len);
 }
--- ft_tests.c
+++ ft_tests.c
@@ -6,17 +6,18 @@
 /*   By: rboudwin <rboudwin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/09 16:13:53 by rboudwin          #+#    #+#             */
-/*   Updated: 2023/11/13 14:05:07 by rboudwin         ###   ########.fr       */
+/*   Updated: 2023/11/13 15:14:25 by rboudwin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft_ftprintf.h"
+#include "ft_printf.h"
 #include <stdio.h>
 
 int	main(void)
 {
 	ft_printf("Let's try a string alone");
 	ft_printf("Let's try a format string with an integer '%d' and some text after", 42);
+//	ft_printf("Let's try three strings '%s'", "one");
 	/*
 	if (argc == 1)
 		ft_printf("Please enter arguments to be sent to the printf function");
