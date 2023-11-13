6d3c1faf2e15e8efd3d2536f7a4abe0f02e7e499
Ryan Boudwin <rboudwin@student.hive.fi>
2023-11-13 14:07:30 +0200

    %d now works

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: rboudwin <rboudwin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/09 15:32:55 by rboudwin          #+#    #+#             */
-/*   Updated: 2023/11/13 12:31:43 by rboudwin         ###   ########.fr       */
+/*   Updated: 2023/11/13 14:06:24 by rboudwin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,24 +32,29 @@
 #include "libft_ftprintf.h"
 #include <stdio.h>
 
-static int	ft_identify_data_type(const char *c, char *str, int i, va_list args)
+static char	*ft_identify_data_type(const char *c, char *str, int *i, va_list args)
 {
-	int	n;
-	printf("We are trying to id data type, i is currently %d and c[i] is '%c'", i, c[i]);
-	i++;
-	if (c[i] == 'd')
+	int		n;
+	char	*tmpstr;
+	char	*itoa_result;
+
+	printf("We are trying to id data type, i is currently %d and c[i] is '%c'", *i, c[*i]);
+	(*i)++;
+
+	if (c[*i] == 'd')
 	{
 		n = va_arg(args, int);
 		printf("n is %d \n", n);
-		str = ft_strjoin(str, ft_itoa(n));
-		return (i + 1);
+		itoa_result = ft_itoa(n);
+		tmpstr = ft_strjoin(str, itoa_result);
+		free(itoa_result);
+		free(str);
+		str = tmpstr;
+		(*i)++;
+		return (str);
 	}
 
-		//ft_fetch_int();
-	//else if (c[i] == 'c')
-	  // ft_fetch_char();
-
-	return (i);
+	return (str);
 }
 int ft_printf(const char *c, ...)
 {
@@ -57,8 +62,10 @@ int ft_printf(const char *c, ...)
 	int		start;
 	int		len;
 	char	*str;	
+	char	*sub_str;
 	va_list	args;
-	//printf("%s", c);
+	char	*tmpstr;
+
 	i = 0;
 	start = 0;
 	len = 0;
@@ -73,27 +80,38 @@ printf("ft_printf launched, str is now '%s' \n", str);
 		if (c[i] == '%')
 		{
 			printf("we entered the percent loop \n");
-			str = ft_strjoin(str, ft_substr(c, start, len)); 
+			sub_str = ft_substr(c, start, len);
+			tmpstr = ft_strjoin(str, sub_str);
+			free(sub_str); 
+			free(str);
+			if (!tmpstr)
+				return (-1);
+			str = tmpstr;
+			free(tmpstr);
 			// If we decide to write logic to handle flags, we will need to wrap the next line with a
 			// flag handler
-			va_arg(args, char *);
-			i = ft_identify_data_type(c, str, i, args);
+			//va_arg(args, char *);
+			tmpstr = ft_identify_data_type(c, str, &i, args);
+			if (!tmpstr)
+				return (-1);
+			str = tmpstr;
+			c = &c[i];
 		}
 		else
 			len++;
 		i++;
 	}
 	printf("We exited format string loop, i is '%d' and str is '%s' \n", i, str);
+	tmpstr = ft_strjoin(str, (char *)c);
 	if (str[0] == '\0')
-	{
-		printf("We concluded str was never changed so we are just copying c \n");
-		str = ft_strjoin(str, (char *)c);
-	}
+		free(str);
+	str = tmpstr;
 	printf("We are done checking to see if str still has a null terminator \n");
 	ft_putstr_fd(str, 1);
 	// printf("\n It works %s\n", str);
 	// ft_putstr_fd(str, 1);
 	va_end(args);
+	len = ft_strlen(str);
 	free(str);
-	return (ft_strlen(str));
+	return (len);
 }
--- ft_tests.c
+++ ft_tests.c
@@ -6,15 +6,18 @@
 /*   By: rboudwin <rboudwin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/09 16:13:53 by rboudwin          #+#    #+#             */
-/*   Updated: 2023/11/13 11:46:12 by rboudwin         ###   ########.fr       */
+/*   Updated: 2023/11/13 14:05:07 by rboudwin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft_ftprintf.h"
 #include <stdio.h>
 
-int	main(int argc, char **argv)
+int	main(void)
 {
+	ft_printf("Let's try a string alone");
+	ft_printf("Let's try a format string with an integer '%d' and some text after", 42);
+	/*
 	if (argc == 1)
 		ft_printf("Please enter arguments to be sent to the printf function");
 	else if (argc == 2){
@@ -24,4 +27,5 @@ int	main(int argc, char **argv)
 		ft_printf(argv[1], argv[2]);
 	else
 		ft_printf("Too many arguments for now.");
+		*/
 }
