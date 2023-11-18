38fd0b70cd88e2cc425b4eb6f59c87ec527b2cb9
Ryan Boudwin <rboudwin@student.hive.fi>
2023-11-18 16:52:27 +0200

    fixed norm error in ft_tests.c

--- ft_tests.c
+++ ft_tests.c
@@ -6,7 +6,7 @@
 /*   By: rboudwin <rboudwin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/09 16:13:53 by rboudwin          #+#    #+#             */
-/*   Updated: 2023/11/18 16:46:39 by rboudwin         ###   ########.fr       */
+/*   Updated: 2023/11/18 16:52:02 by rboudwin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,11 +25,12 @@ int	main(void)
 	a = 3;
 	ptr = &a;
 	ft_printf("Let's try a string alone \n");
-	ft_printf("Let's try a format string with an integer '%d' and a percent sign %% after \n", 42);
+	ft_printf("'%d' and a percent sign %% after \n", 42);
 	ft_printf("%c%c%c\n", 'a', 'b', 'c');
 	ft_return = ft_printf("'%u'\n", -10);
 	printf_return = printf("'%u'\n", -10);
-	printf("Return values - ft: '%d' printf: '%d'\n", ft_return, printf_return);
+	printf("Return values - ft: '%d' 
+	printf: '%d'\n", ft_return, printf_return);
 	ft_printf("Let's try three strings '%s' '%s' '%s'\n",
 	  "one", "two", "three");
 	ft_printf("Let's try some chars '%c' '%c'\n", 'a', 'b');
@@ -52,17 +53,4 @@ int	main(void)
 	printf("%p\n", (void *)-14523);
 //	ft_printf("Max long: %d", 9223372036854775807);
 //	printf("Max long: %d", 9223372036854775807);
-	
-/*	
-	if (argc == 1)
-		ft_printf("Please enter arguments to be sent to the printf function");
-	else if (argc == 2){
-		printf("We concluded we have one argument 
-		and it should print it as a string \n");
-		ft_printf(argv[1]);}
-	else if (argc == 3)
-		ft_printf(argv[1], argv[2]);
-	else
-		ft_printf("Too many arguments for now.");
-*/	
-//}
+}*/	
